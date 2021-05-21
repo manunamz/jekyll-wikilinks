57a78a395aa4537e730afa46e2257da757dcd226
shorty25h0r7 <short2thingz@gmail.com>
2021-05-21 11:32:42 -0400

    backlinks_data: add 'backlink_type' 'filter' so users can filter backlinks by jekyll type.

--- lib/jekyll-wikilinks.rb
+++ lib/jekyll-wikilinks.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require "jekyll"
 require_relative "jekyll-wikilinks/context"
+require_relative "jekyll-wikilinks/filter"
 require_relative "jekyll-wikilinks/version"
 
 
