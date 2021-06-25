b7c9abde6a9363e20f5d77a83684ffa7a684d8ac
manunamz <manunamz@pm.me>
2021-06-25 13:00:42 -0400

    ref_regex: update filters so attrs/links may be filtered by jekyll doc type or link type (there are probably bugs); made some progress on the tests, but they still aren't up and running yet.

--- lib/jekyll-wikilinks/filter.rb
+++ lib/jekyll-wikilinks/filter.rb
@@ -1,16 +1,29 @@
 # frozen_string_literal: true
 
 module JekyllWikiLinks
-  module BackLinkTypeFilters
-    # usage:
-    # {% assign note_backlinks = page.backlinks | backlink_type = "notes" %}
-    def backlink_type(backlinks, type)
-      return if backlinks.nil?
-      target_backlinks = []
-      backlinks.each do |bl|
-        target_backlinks << bl if self.to_string(bl.type) == type
+  module TypeFilters
+    # 'links' accepts both untyped links, typed links, and attributes; fore and back.
+
+    # usage: {% assign note_links = page.links | doc_type = "notes" %}
+    # "doc_type" is the jekyll type ("pages", "posts", "<collection-name>")
+    def doc_type(links, doc_type)
+      return if links.nil?
+      target_links = []
+      links.each do |l|
+        target_links << l if self.to_string(l['doc'].type) == doc_type.to_str
       end
-      return target_backlinks
+      return target_links.uniq
+    end
+
+    # usage: {% assign author_links = page.links | link_type = "author" %}
+    # "link_type" is the wikilink's type, the string that appears before the link in `link-type::[[wikilink]]`.
+    def link_type(links, link_type)
+      return if links.nil?
+      target_links = []
+      link.each do |l|
+        target_links << l if self.to_string(l['type'].to_str) == link_type.to_str
+      end
+      return target_links.uniq
     end
 
     def to_string(type)
@@ -25,4 +38,4 @@ module JekyllWikiLinks
   end
 end
 
-Liquid::Template.register_filter(JekyllWikiLinks::BackLinkTypeFilters)
+Liquid::Template.register_filter(JekyllWikiLinks::TypeFilters)
---
+++ spec/jekyll-wikilinks/feature_filter_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require "jekyll"
+require "jekyll-wikilinks"
+require "spec_helper"
+
+RSpec.describe(JekyllWikiLinks::TypeFilters) do
+
+  let(:doc)        { instance_double("Document", :collection => "docs", :content => "a document", :title=> "A Document") }
+  let(:post)       { instance_double("Document", :collection => "posts", :content => "a post", :title=>"A Post") }
+  let(:page)       { instance_double("Page", :relative_path => "_pages/a-page.md", :content => "a page", :title=>"A Page") }
+  let(:mock_links) { [{ :doc => doc }, { :doc => post }, { :doc => page }] }
+  # let(:mock_links) { [doc, post, page] }
+
+  # let(:filtered_by_doc_type)  {  }
+  # let(:filtered_by_link_type) {  }
+
+  context "filter by 'doc_type'" do
+    let(:content)    { '{% assign note_links = mock_links | doc_type: "docs" %}{% for link in note_links %}{{ link.doc.title }}{% endfor %}' }
+    let(:output) do
+      render_liquid(content, { 'mock_links' => mock_links })
+    end
+
+    it "filters links by jekyll doc type" do
+      pending("figure out how to test liquid template filters")
+      # expect(output).to eq([{ :doc => doc }])
+      expect(1).to eq(2)
+    end
+  end
+
+  context "filter by 'link_type'" do
+
+    it "filters links by 'link_type'" do
+      pending("figure out how to test liquid template filters")
+      expect(1).to eq(2)
+    end
+
+  end
+end
