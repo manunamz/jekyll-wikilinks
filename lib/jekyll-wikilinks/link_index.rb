2dfb286e468921e37110f3c31349081dd1dbcada
manunamz <manunamz@pm.me>
2021-08-13 14:15:52 -0400

    ref_link - ref: add missing forelinks to linkindex (so d3 plugin doesn't have to mess with regex).

--- lib/jekyll-wikilinks/link_index.rb
+++ lib/jekyll-wikilinks/link_index.rb
@@ -1,3 +1,5 @@
+require 'jekyll-wikilinks/naming_const' # for REGEX_NOT_GREEDY
+
 module Jekyll
   module WikiLinks
 
@@ -7,7 +9,9 @@ module Jekyll
       # Use Jekyll's native relative_url filter
       include Jekyll::Filters::URLFilters
 
-      REGEX_LINK_TYPE = /<a\sclass="wiki-link(\slink-type\s(?<link-type>([^"]+)))?"\shref="(?<link-url>([^"]+))">/i
+      # identify missing links in doc via .invalid-wiki-link class and nested doc-text.
+      REGEX_INVALID_WIKI_LINK = /invalid-wiki-link#{REGEX_NOT_GREEDY}\[\[(#{REGEX_NOT_GREEDY})\]\]/i
+      REGEX_VALID_WIKI_LINK = /<a\sclass="wiki-link(\slink-type\s(?<link-type>([^"]+)))?"\shref="(?<link-url>([^"]+))">/i
 
       def initialize(site, doc_manager)
         @context ||= Jekyll::WikiLinks::Context.new(site)
@@ -26,6 +30,7 @@ module Jekyll
           doc.data['attributes'] = @index[doc.url].attributes
           doc.data['backlinks']  = @index[doc.url].backlinks
           doc.data['forelinks']  = @index[doc.url].forelinks
+          doc.data['missing']    = @index[doc.url].missing
         end
       end
 
@@ -51,7 +56,7 @@ module Jekyll
         # for each document...
         @doc_manager.all.each do |doc|
           # ...process its forelinks
-          doc.content.scan(REGEX_LINK_TYPE).each do |m|
+          doc.content.scan(REGEX_VALID_WIKI_LINK).each do |m|
             ltype, lurl = m[0], m[1]
             @index[doc.url].forelinks << {
               'type' => ltype,
@@ -60,7 +65,7 @@ module Jekyll
           end
           # ...process its backlinks
           @doc_manager.all.each do |doc_to_backlink|
-            doc_to_backlink.content.scan(REGEX_LINK_TYPE).each do |m|
+            doc_to_backlink.content.scan(REGEX_VALID_WIKI_LINK).each do |m|
               ltype, lurl = m[0], m[1]
               if lurl == relative_url(doc.url)
                 @index[doc.url].backlinks << {
@@ -69,18 +74,23 @@ module Jekyll
                 }
               end
             end
+            doc_to_backlink.content.scan(REGEX_INVALID_WIKI_LINK).each do |m|
+              ltext = m[0]
+              @index[doc.url].missing << ltext
+            end
           end
         end
       end
 
       class LinksInfo
-        attr_accessor :attributes, :attributed, :backlinks, :forelinks
+        attr_accessor :attributes, :attributed, :backlinks, :forelinks, :missing
 
         def initialize
           @attributed = [] # block typed backlinks
           @attributes = [] # block typed forelinks
           @backlinks  = []
           @forelinks  = []
+          @missing    = [] # missing forelinks
         end
       end
     end
