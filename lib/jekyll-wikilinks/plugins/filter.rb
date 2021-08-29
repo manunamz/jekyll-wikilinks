d2cddbbee0bffe1462d4fc122cac86831d8ebec4
manunamz <manunamz@pm.me>
2021-08-29 14:35:09 -0400

    add_blk_lst: fix filters to support 'attributes'.

--- lib/jekyll-wikilinks/plugins/filter.rb
+++ lib/jekyll-wikilinks/plugins/filter.rb
@@ -4,7 +4,7 @@ module Jekyll
   module WikiLinks
 
     module TypeFilters
-      # 'links' accepts both untyped links, typed links, and attributes; fore and back.
+      # 'links' accepts untyped links, typed links, and attributes; fore and back.
 
       # usage: {% assign note_links = page.links | doc_type = "notes" %}
       def doc_type(links, doc_type)
@@ -14,27 +14,51 @@ module Jekyll
 
         site = @context.registers[:site]
         links.each do |l|
-          doc = site.documents.select{ |d| d.url == l['url'] && d.type.to_s == doc_type.to_s }
-          if doc.nil? || doc.size != 1
-            links.delete(l)
+          # links
+          if l.keys.include?('url')
+            doc = site.documents.select{ |d| d.url == l['url'] && d.type.to_s == doc_type.to_s }
+            if !doc.nil? && doc.size != 1
+              links.delete(l)
+            end
+          # attributes
+          elsif l.keys.include?('urls')
+            l['urls'].each do |lurl|
+              doc = site.documents.select{ |d| d.url == lurl && d.type.to_s == doc_type.to_s }
+              if !doc.nil? && doc.size != 1
+                links['urls'].delete(lurl)
+              end
+            end
+          else
+            Jekyll.logge.error("In 'doc_type' filter, 'links' do not have 'url' or 'urls'")
           end
         end
         return links.uniq
       end
 
-      # usage: {% assign author_links = page.links | link_type = "author" %}
-      def link_type(links, link_type)
+      # usage: {% assign author_links = page.links | rel_type = "author" %}
+      def rel_type(links, link_type)
         Jekyll.logger.error("'links' should not be nil") if links.nil?
         return "No link type given" if link_type.empty?
         return [] if links.empty?
 
         site = @context.registers[:site]
         links.each do |l|
-          if l['type'].to_s == link_type.to_s
-            docs = site.documents.select{ |d| d.url == l['url'] }
-            if !docs.nil? && docs.size == 1
-              links.delete(l)
+          if l.keys.include?('url')
+            if l['type'].to_s == link_type.to_s
+              docs = site.documents.select{ |d| d.url == l['url'] }
+              if !doc.nil? && doc.size != 1
+                links.delete(l)
+              end
             end
+          elsif l.keys.include?('urls')
+            l['urls'].each do |lurl|
+              docs = site.documents.select{ |d| d.url == lurl }
+              if !doc.nil? && doc.size != 1
+                links['urls'].delete(lurl)
+              end
+            end
+          else
+            Jekyll.logge.error("In 'rel_type' filter, 'links' do not have 'url' or 'urls'")
           end
         end
         return links.uniq
