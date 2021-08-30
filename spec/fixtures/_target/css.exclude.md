8f28be9fd57ceef90ca0dfa6ebbbbd879bb14c76
manunamz <manunamz@pm.me>
2021-08-30 16:07:56 -0400

    ref_test: ref config tests.

---
+++ spec/fixtures/_target/css.exclude.md
@@ -0,0 +1,5 @@
+---
+title: Excluded CSS
+---
+
+An <a class="exclude-link" href="www.example.com">excluded css class</a>.
--- spec/jekyll-wikilinks/feature_config_spec.rb
+++ spec/jekyll-wikilinks/feature_config_spec.rb
@@ -6,13 +6,15 @@ require "shared_context"
 
 RSpec.describe(Jekyll::WikiLinks) do
   include_context "shared jekyll configs"
-  let(:config_overrides)                { {} }
+
   let(:site)                            { Jekyll::Site.new(config) }
 
-  let(:base_case_a)                     { find_by_title(site.collections["docs"].docs, "Base Case A") }
-  let(:base_case_b)                     { find_by_title(site.collections["docs"].docs, "Base Case B") }
+  # links
+  let(:inline_untyped_link)             { find_by_title(site.collections["untyped"].docs, "Untyped Link") }
+  # targets
+  let(:blank_a)                         { find_by_title(site.collections["target"].docs, "Blank A") }
+  let(:css_exclude)                     { find_by_title(site.collections["target"].docs, "Excluded CSS") }
   let(:one_page)                        { find_by_title(site.pages, "One Page") }
-  let(:web_link_exclude)                { find_by_title(site.collections["docs"].docs, "Excluded Web Link") }
 
   # makes markdown tests work
   subject { described_class.new(site.config) }
@@ -27,47 +29,62 @@ RSpec.describe(Jekyll::WikiLinks) do
     FileUtils.rm_rf(Dir["#{site_dir()}"])
   end
 
-  context "when 'baseurl' is set in configs" do
-    let(:config_overrides) { { "baseurl" => "/wikilinks" } }
+  context "CONFIG [[wikilinks]]" do
 
-    it "baseurl included in href" do
-      expect(base_case_a.output).to include("/wikilinks")
+    context "when 'baseurl' is set in configs" do
+      let(:config_overrides) { {
+        "collections" => { "untyped" => { "output" => true }, "target" => { "output" => true } },
+        "baseurl" => "/wikilinks",
+      } }
+
+      it "baseurl included in href" do
+        expect(inline_untyped_link.output).to include("/wikilinks")
+      end
+
+      it "wiki-links are parsed and 'a' tag is generated" do
+        expect(inline_untyped_link.output).to eq("<p>This doc contains a wikilink to <a class=\"wiki-link\" href=\"/wikilinks/target/blank.a/\">blank a</a>.</p>\n")
+      end
+
     end
 
-    it "wiki-links are parsed and a element is generated" do
-      expect(base_case_a.output).to eq("<p>This <a class=\"wiki-link\" href=\"/wikilinks/doc/e0c824b6-0b8c-4595-8032-b6889edd815f/\">base case b</a> has a littlecar.</p>\n")
+    context "when jekyll-wikilinks is disabled in configs" do
+      let(:config_overrides) { {
+        "collections" => { "untyped" => { "output" => true }, "target" => { "output" => true } },
+        "wikilinks" => { "enabled" => false },
+      } }
+
+      it "does not process [[wikilinks]]" do
+        expect(inline_untyped_link.output).to include("[[blank.a]]")
+      end
+
     end
 
-  end
+    context "when certain jekyll types are excluded in configs" do
+      let(:config_overrides) { {
+        "collections" => { "untyped" => { "output" => true }, "target" => { "output" => true } },
+        "wikilinks" => { "exclude" => ["untyped", "pages", "posts"] },
+      } }
 
-  context "when jekyll-wikilinks is disabled in configs" do
-    let(:config_overrides) { { "wikilinks" => { "enabled" => false } } }
+      it "does not process [[wikilinks]] for those types" do
+        expect(inline_untyped_link.output).to include("[[blank.a]]")
+      end
 
-    it "does not process [[wikilinks]]" do
-      expect(base_case_a.content).to include("[[base-case.b]]")
     end
 
-  end
+    context "when css classes are excluded" do
+      let(:config_overrides) { {
+        "collections" => { "untyped" => { "output" => true }, "target" => { "output" => true } },
+        "wikilinks" => { "css" => { "exclude" => [ "exclude-link" ] } },
+      } }
 
-  context "when certain jekyll types are excluded in configs" do
-    let(:config_overrides) { { "wikilinks" => { "exclude" => ["docs", "pages", "posts"] } } }
+      it "does not classify 'a' tags as web links with excluded css classes" do
+        expect(css_exclude.output).to_not include("web-link")
+      end
 
-    it "does not process [[wikilinks]] for those types" do
-      expect(base_case_a.content).to include("[[base-case.b]]")
-      expect(one_page.content).to include("[[base-case.a]]")
-    end
+      it "full output" do
+        expect(css_exclude.output).to eq("<p>An <a class=\"exclude-link\" href=\"www.example.com\">excluded css class</a>.</p>\n")
+      end
 
-  end
-
-  context "when css classes are excluded" do
-    let(:config_overrides) { { "wikilinks" => { "css" => { "exclude" => [ "exclude-link" ] } } } }
-
-    it "does not classify 'a' tags as web links with excluded css classes" do
-      expect(web_link_exclude.content).to_not include("web-link")
-    end
-
-    it "full output" do
-      expect(web_link_exclude.content).to eq("<p>An <a class=\"exclude-link\" href=\"www.example.com\">excluded link</a>.</p>\n")
     end
 
   end
