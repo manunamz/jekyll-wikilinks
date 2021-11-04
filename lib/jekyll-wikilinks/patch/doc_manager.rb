193bc2abb331cc055bec7d2b327f40f2809529a7
manunamz <manunamz@pm.me>
2021-11-04 13:27:35 -0400

    ref_miss: have 'doc_manager' throw errors if methods are used improperly (given empty strings, etc.).

--- lib/jekyll-wikilinks/patch/doc_manager.rb
+++ lib/jekyll-wikilinks/patch/doc_manager.rb
@@ -7,8 +7,21 @@ module Jekyll
     #
     # this class is responsible for answering any questions
     # related to jekyll markdown documents
-    # that are meant to be processed by the wikilinks plugin
+    # that are meant to be processed by the wikilinks plugin.
     #
+    # the following methods are specifically to address two things:
+    #  1. ruby's 'find' / 'detect' function does not throw errors if 
+    #     there are multiple matches. fail fast, i want to know if there
+    #     are duplicates.
+    #  2. handle all jekyll documents in one place. i don't want to
+    #     have to filter all documents for target markdown documents 
+    #     every time i need to check if a file exists.
+    # 
+    # there is probably a better way to do this...i would prefer to have
+    # a plugin-wide function that just wraps all of this and can be called
+    # from anywhere in the plugin...but ruby is not a functional language...
+    # gotta have classes...
+    # 
     class DocManager
       CONVERTER_CLASS = Jekyll::Converters::Markdown
 
@@ -35,30 +48,31 @@ module Jekyll
       end
 
       def get_doc_by_fname(filename)
-        return nil if filename.nil? || @md_docs.size == 0
+        Jekyll.logger.error "Must provide a 'filename'" if filename.nil? || filename.empty?
         docs = @md_docs.select{ |d| File.basename(d.basename, File.extname(d.basename)) == filename }
-        return nil if docs.nil? || docs.size > 1
+        return nil if docs.nil? || docs.empty? || docs.size > 1
         return docs[0]
       end
 
       def get_doc_by_url(url)
-        return nil if url.nil? || @md_docs.size == 0
+        Jekyll.logger.error "Must provide a 'url'" if url.nil? || url.empty?
         docs = @md_docs.select{ |d| d.url == url }
-        return nil if docs.nil? || docs.size > 1
+        return nil if docs.nil? || docs.empty? || docs.size > 1
         return docs[0]
       end
 
       def get_doc_content(filename)
-        return nil if filename.nil? || @md_docs.size == 0
+        Jekyll.logger.error "Must provide a 'filename'" if filename.nil? || filename.empty?
         docs = @md_docs.select{ |d| File.basename(d.basename, File.extname(d.basename)) == filename }
         return docs[0].content if docs.size == 1
         return nil
       end
 
       def get_image_by_fname(filename)
-        return nil if filename.nil? || @static_files.size == 0 || !SUPPORTED_IMG_FORMATS.any?{ |ext| ext == File.extname(filename).downcase }
+        Jekyll.logger.error "Must provide a 'filename'" if filename.nil? || filename.empty?
+        return nil if @static_files.size == 0 || !SUPPORTED_IMG_FORMATS.any?{ |ext| ext == File.extname(filename).downcase }
         docs = @static_files.select{ |d| File.basename(d.relative_path) == filename }
-        return nil if docs.nil? || docs.size > 1
+        return nil if docs.nil? || docs.empty? || docs.size > 1
         return docs[0]
       end
 
@@ -73,7 +87,7 @@ module Jekyll
       end
 
       def doc_has_header?(doc, header)
-        return nil if header.nil?
+        Jekyll.logger.error "Must provide a 'header'" if header.nil? || header.empty?
         # leading + trailing whitespace is ignored when matching headers
         header_results = doc.content.scan(REGEX_ATX_HEADER).flatten.map { |htxt| htxt.strip }
         setext_header_results = doc.content.scan(REGEX_SETEXT_HEADER).flatten.map { |htxt| htxt.strip }
@@ -81,7 +95,7 @@ module Jekyll
       end
 
       def doc_has_block_id?(doc, block_id)
-        return nil if block_id.nil?
+        Jekyll.logger.error "Must provide a 'block_id'" if block_id.nil? || block_id.empty?
         # leading + trailing whitespace is ignored when matching blocks
         block_id_results = doc.content.scan(REGEX_BLOCK).flatten.map { |bid| bid.strip }
         return block_id_results.include?(block_id)
